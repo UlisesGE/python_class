8c9f55a1f2fe33b7d79ca506cf32f2ed4d0408ff
Ulises Gaspar <hectorgasp@gmail.com>
2022-03-23 20:21:49 -0600

    Se agrega ejercicio para entregar: cuenta de nucleotidos

---
+++ counting_nucleotides.py
@@ -0,0 +1,6 @@
+secuencia = input('Introduce una secuencia:\n')
+
+print("Numero de G:", secuencia.count("G"))
+print("Numero de A:", secuencia.count("A"))
+print("Numero de T:", secuencia.count("T"))
+print("Numero de C:", secuencia.count("C"))
