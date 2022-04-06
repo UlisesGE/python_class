fccf5138aef650cc702286abaf46bb5bf6ba6d3d
Ulises Gaspar <hectorgasp@gmail.com>
2022-04-05 23:23:48 -0500

    Se actualiza script ATGC percentage y se agrega archivo de prueba

--- src/ATGC-percentage.py
+++ src/ATGC-percentage.py
@@ -23,6 +23,18 @@ USAGE
         
 '''
 #Se obtiene el nombre del archivo de la secuencia: 
-file_name: = input("Escribe el nombre de tu archivo y su posicion: ")
+file_name = input("Escribe el nombre de tu archivo y su posicion: ")
 
+with open(file_name, 'r') as file:
+        sequence = file.read()
 
+#Se elimina el salto de linea 
+sequence.rstrip("\n")
+
+#Se obtiene la longitud de la secuencia
+seq_length = len(sequence)
+
+#Se obtienen los porcentajes
+print(f"Porcentajes: ")
+print(f"AT = {((sequence.count('A') + sequence.count('T')) / seq_length) * 100} %")
+print(f"GC = {((sequence.count('G') + sequence.count('C')) / seq_length) * 100} %")
