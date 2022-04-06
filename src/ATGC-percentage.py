d6e301dd909516cddf4d6fcf430137fca5858a5b
Ulises Gaspar <hectorgasp@gmail.com>
2022-04-05 22:06:35 -0500

    Se agrega version preliminar de porcentaje ATCG

---
+++ src/ATGC-percentage.py
@@ -0,0 +1,28 @@
+'''NAME
+       AT-percentage
+
+VERSION
+        1.0
+
+AUTHOR
+        Hector Ulises
+
+DESCRIPTION
+        Calculate percentage of AT on a genome sequence
+
+CATEGORY
+        Genomic sequence
+
+USAGE
+
+    % python AT-percentage.py -i filename
+    
+    example
+    
+    % python AT-percentage.py -i sequence.txt
+        
+'''
+#Se obtiene el nombre del archivo de la secuencia: 
+file_name: = input("Escribe el nombre de tu archivo y su posicion: ")
+
+
