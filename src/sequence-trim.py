a451c002244c8256ab099c7fa5f3c61a43e33ae4
Ulises Gaspar <hectorgasp@gmail.com>
2022-04-10 14:49:23 -0500

    Se agregan comentarios a sequence trim

--- src/sequence-trim.py
+++ src/sequence-trim.py
@@ -2,7 +2,7 @@
        sequence-trim.py
 
 VERSION
-        1.0
+        1.1
 
 AUTHOR
         Hector Ulises Gaspar <hectorgasp@gmail.com>
@@ -27,12 +27,14 @@ SEE ALSO
 
 '''
 
+#se abre el archivo de las secuencias, y se guerdan en una lista
 with open('4_input_adapters.txt','r') as file:
         adapters = file.readlines()
 
-#print(adapters)
+#Se crea un archivo nuevo, donde se guardarán las secuencias recortadas
 trimmed = open('trimmed.txt', "w")
 
+#se hace un ciclo for que recortará cada secuencia, y las guardará en un archivo final
 for adapter in adapters:
         trimmed_seq = adapter[14:]
         trimmed.write(trimmed_seq)
