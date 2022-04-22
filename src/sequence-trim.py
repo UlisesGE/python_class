5adaecb2909ffcd49ab56b92d43dd577c650a275
MichMourra <60996279+MichMourra@users.noreply.github.com>
2022-04-22 01:27:42 -0500

    Sugerencia DNA-to-FASTA.py
    Muy buen código y lógica :) , Solo te sugiero que seas muy cuidadoso con cerrar los archivos que abres, ya que de lo contrario tu resultado no se vera reflejado. Tambien espero que agregues las carpetas faltantes a tu repositorio.

--- src/sequence-trim.py
+++ src/sequence-trim.py
@@ -1,40 +1,32 @@
-'''NAME
+'''
+NAME
        sequence-trim.py
-
 VERSION
         1.1
-
 AUTHOR
         Hector Ulises Gaspar <hectorgasp@gmail.com>
-
 DESCRIPTION
-        Este programa recortará la porción del adaptador de una secuencia dada, y lo pondrá en un archivo nuevo llamado trimmed.txt
-
+        Este programa recortará la porción del adaptador de una secuencia dada, 
+        y lo pondrá en un archivo nuevo llamado trimmed.txt
 CATEGORY
         dna sequence
-
 USAGE
          py src/DNA-to-FASTA.py
-
-
 ARGUMENTS
          none
-
     
-SEE ALSO
-        
-
-
+SEE ALSO      
 '''
 
 #se abre el archivo de las secuencias, y se guerdan en una lista
-with open('4_input_adapters.txt','r') as file:
+with open('../data/4_input_adapters.txt','r') as file:
         adapters = file.readlines()
 
 #Se crea un archivo nuevo, donde se guardarán las secuencias recortadas
-trimmed = open('trimmed.txt', "w")
+trimmed = open('../results/trimmed.txt', "w")
 
 #se hace un ciclo for que recortará cada secuencia, y las guardará en un archivo final
 for adapter in adapters:
         trimmed_seq = adapter[14:]
         trimmed.write(trimmed_seq)
+trimmed.close()
