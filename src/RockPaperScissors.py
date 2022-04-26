475fb7065f32c1af923629e30c8f22e7aa0c6b93
MichMourra <60996279+MichMourra@users.noreply.github.com>
2022-04-26 12:15:55 -0500

    Sugerencia rockpaperscissors.py
    El código esta muy bien corre perfectamente, solo te falta cumplir con algunos apartados de la rubrica. 1. Cada cambio que hagas en el código recuerda registrarlo con commits para que quede guardado en el historial de cambios. 2. Recuerda que lo recomendable es que cada bloque de código este documentado, no basta unicamente con el encabezado. 3. Recuerda tener los directorios necesarios para el repositorio en tu caso hacen falta las carpetas data y results.

--- src/RockPaperScissors.py
+++ src/RockPaperScissors.py
@@ -15,47 +15,55 @@ CATEGORY
 	Recreación
     
 USAGE
-
     py src/rockpaperscissors.py
     
 ARGUMENTS
     none
-
     
 SEE ALSO
         
-
 ''' 
 import random
 
+# Pedir el nombre del usuario
 print("¡El ganador es el primero en juntar 2 puntos!")
 name = input("Dime tu nombre:")
 
+# Inicializar listas y variables a ocupar
 choices = ("piedra", "papel", "tijeras")
 computer_score = 0
 user_score = 0
 
+# Obtener elección del usuario y de la computadora
 user_choice = input(f"Ingresa una opcion, {name}: piedra, papel o tijeras \n").lower()
 computer_choice = random.choice(choices)
 
+# Si la opción no es valida, avisar y cerrar el programa
+if user_choice not in choices:
+    print("Error, opción no válida vuelve a intentarlo")
+    exit()
+
+# Si las opciones son iguales, sera empate
 if computer_choice == user_choice:
     print(f"El oponente eligió {computer_choice}. Empate, chanclas")
-          
+
+# Primer caso elección piedra
 elif user_choice == "piedra":
     if computer_choice == "papel":
         print(f"El oponente eligió {computer_choice}. Perdiste banda :(")
     if computer_choice == "tijeras":
         print(f"El oponente eligió {computer_choice}. ¡Ganaste!")
 
+# Segundo caso elección tijeras
 elif user_choice == "tijeras":
     if computer_choice == "piedra":
         print(f"El oponente eligió {computer_choice}. Perdiste banda :(")
     if computer_choice == "papel":
         print(f"El oponente eligió {computer_choice}. ¡Ganaste!")
-
+        
+# Tercer caso elección papel
 elif user_choice == "papel":
     if computer_choice == "tijeras":
         print(f"El oponente eligió {computer_choice}. Perdiste banda :(")
     if computer_choice == "piedra":
         print(f"El oponente eligió {computer_choice}. ¡Ganaste!")
-
