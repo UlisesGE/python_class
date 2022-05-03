02a01de630c1219a688a4dd2ae698682fc556e29
UlisesGE <100377607+UlisesGE@users.noreply.github.com>
2022-05-02 20:27:06 -0500

    c elimina codigo repetido

--- counting_nucleotides.py
+++
@@ -1,6 +0,0 @@
-secuencia = input('Introduce una secuencia:\n')
-
-print("Numero de G:", secuencia.count("G"))
-print("Numero de A:", secuencia.count("A"))
-print("Numero de T:", secuencia.count("T"))
-print("Numero de C:", secuencia.count("C"))
