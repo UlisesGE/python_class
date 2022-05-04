95909968a9290b7606e81693474b7c76ddc00f5d
Ulises Gaspar <hectorgasp@gmail.com>
2022-05-03 21:02:08 -0500

    Se agrega uso de excepciones en el archivo sequence trim

--- src/sequence-trim.py
+++ src/sequence-trim.py
@@ -19,14 +19,18 @@ SEE ALSO
 '''
 
 #se abre el archivo de las secuencias, y se guerdan en una lista
-with open('../data/4_input_adapters.txt','r') as file:
-        adapters = file.readlines()
+#Se agrega una excepcion en caso de que el archivo no se encuente en la ruta especificada por el ejercicio
+try:
+        with open('../data/4_input_adapters.txt','r') as file:
+                adapters = file.readlines()
+except IOError as io_error:
+        print(f"No se encontró {io_error.filrname} en la ruta especificada")
+else:
+        #Se crea un archivo nuevo, donde se guardarán las secuencias recortadas
+        trimmed = open('../results/trimmed.txt', "w")
 
-#Se crea un archivo nuevo, donde se guardarán las secuencias recortadas
-trimmed = open('../results/trimmed.txt', "w")
-
-#se hace un ciclo for que recortará cada secuencia, y las guardará en un archivo final
-for adapter in adapters:
-        trimmed_seq = adapter[14:]
-        trimmed.write(trimmed_seq)
-trimmed.close()
+        #se hace un ciclo for que recortará cada secuencia, y las guardará en un archivo final
+        for adapter in adapters:
+                trimmed_seq = adapter[14:]
+                trimmed.write(trimmed_seq)
+        trimmed.close()
