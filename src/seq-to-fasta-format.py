f7f6dd3d5fb6261ca0d9a005576650eed0d727e9
Ulises Gaspar <hectorgasp@gmail.com>
2022-05-03 21:09:02 -0500

    Se agrega excepcion a seq to fasta

--- src/seq-to-fasta-format.py
+++ src/seq-to-fasta-format.py
@@ -18,20 +18,24 @@ ARGUMENTS
 SEE ALSO   
 '''
 
-# Leer las secuencias y guardarlas
-with open('../data/dna_sequences.txt','r') as file:
-        sequences = file.readlines()
+#Se agrega una excepcion en caso de que el archivo no se encuente en la ruta
+try:
+        # Leer las secuencias y guardarlas
+        with open('../data/dna_sequences.txt','r') as file:
+                sequences = file.readlines()
+except IOError as io_error:
+        print(f"El archivo {io_error.filename} no se enconto en la ruta especificada")
+else:
+        # Abrir el archivo de escritura
+        FASTA = open("../results/FASTA-formated.fasta", "w")
 
-# Abrir el archivo de escritura
-FASTA = open("../results/FASTA-formated.fasta", "w")
+        # Eliminar los saltos de linea y dar el formato de escritura
+        for sequence in sequences:
+                seq_and_id = sequence.split(' ')
+                FASTA.write(f">{seq_and_id[0]}\n")
+                FASTA.write(f"{seq_and_id[-1].upper().replace('-', '')}")
 
-# Eliminar los saltos de linea y dar el formato de escritura
-for sequence in sequences:
-    seq_and_id = sequence.split(' ')
-    FASTA.write(f">{seq_and_id[0]}\n")
-    FASTA.write(f"{seq_and_id[-1].upper().replace('-', '')}")
-
-# Cerrar el archivo e imprimir mensaje
-FASTA.close()
-print("Archivo Fasta generado exitosamente!")
+        # Cerrar el archivo e imprimir mensaje
+        FASTA.close()
+        print("Archivo Fasta generado exitosamente!")
 
