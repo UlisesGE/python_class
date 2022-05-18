cc91a5713b82423cb0c5f695556cd7717fc78ee6
Ulises Gaspar <hectorgasp@gmail.com>
2022-05-18 18:37:37 -0500

    Se agrega el script aminoacid percentage, ya con la funcion

---
+++ src/aminoacid_percentage.py
@@ -0,0 +1,40 @@
+'''NAME
+       aminoacid_percentage
+
+VERSION
+        1.0
+
+AUTHOR
+        Hector Ulises Gaspar <hectorgasp@gmail.com>
+
+DESCRIPTION
+        Este script obtiene el porcentaje de un aminoácido dado de una secuencia dada por el usuario.
+
+CATEGORY
+        Script
+
+USAGE
+         python aminoacid_percentage.py
+
+
+ARGUMENTS
+       
+
+    
+SEE ALSO
+        Ninguno
+
+'''
+
+
+sequence = input('Introduce una secuencia de aminoácidos:')
+
+aminoacid = input('Introduce el aminoácido para obtemer el porcentaje:')
+
+
+def rana(sequence, aminoacid):
+    sequence_length = len(sequence)
+    return (sequence.upper().count(aminoacid.upper())/sequence_length)*100
+
+percentage = rana(sequence, aminoacid)
+print(percentage)
